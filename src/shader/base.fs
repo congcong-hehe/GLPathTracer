a72f41e5f099662366688873cc7cea2ad0b6aca9
congcong-hehe <heyaqihehe@163.com>
2022-04-13 16:46:03 +0800

    feat: hit sphere

--- src/shader/base.fs
+++ src/shader/base.fs
@@ -22,6 +22,22 @@ struct Camera
     vec3 lower_left_corner;
 };
 
+struct Sphere
+{
+    vec3 center;
+    float radius;
+};
+
+bool hitSphere(Ray ray, Sphere sphere)
+{
+    vec3 oc = ray.ori - sphere.center;
+    float a = dot(ray.dir, ray.dir);
+    float b = 2.0 * dot(oc, ray.dir);
+    float c = dot(oc, oc) - sphere.radius * sphere.radius;
+    float discriminant = b * b - 4 * a * c;
+    return discriminant > 0;
+}
+
 uniform Camera camera;
 
 out vec4 FragColor;
@@ -34,5 +50,10 @@ void main()
     ray.ori = camera.ori;
     ray.dir = camera.lower_left_corner + u * camera.horizontal + v * camera.vertical - camera.ori;
     float t = 0.5 * (normalize(ray.dir).y + 1.0);
-    FragColor = vec4((1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0), 1.0);
+    Sphere sphere;
+    sphere.radius = 0.5;
+    sphere.center = vec3(0, 0, -1);
+    if(hitSphere(ray, sphere))
+        FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    else FragColor = vec4((1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0), 1.0);
 }
