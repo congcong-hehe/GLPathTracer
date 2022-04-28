118ceed05c0410be72723d0c6c2708afd5084db2
congcong-hehe <heyaqihehe@163.com>
2022-04-28 15:20:45 +0800

    feat:tonemap

--- src/shader/output_fs.glsl
+++ src/shader/output_fs.glsl
@@ -5,8 +5,14 @@ in vec2 TexCoords;
 
 uniform sampler2D imgTex;
 
+vec3 toneMapping(in vec3 c, float limit) {
+    float luminance = 0.3*c.x + 0.6*c.y + 0.1*c.z;
+    return c * 1.0 / (1.0 + luminance / limit);
+}
+
 void main()
 {
     vec3 color = texture(imgTex, TexCoords).rgb;
+    color = toneMapping(color, 1.5);
     FragColor = vec4(pow(color, vec3(1.0 / 2.2)), 1.0);
 }
